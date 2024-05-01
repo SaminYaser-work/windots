a22a5dce64543ce46da71266330ddf953594a6d8
Samin Yaser <saminyaserwork@gmail.com>
2024-05-01 18:56:09 +0600

    setup script, added more bindings

---
+++ setup.ps1
@@ -0,0 +1,35 @@
+function AddToStartup {
+    param (
+        $target,
+        $name
+    )
+
+    # shell:startup
+    $shortcutPath = [System.Environment]::GetFolderPath('Startup') + "\" + $name + ".lnk"
+
+    # Create shortcut
+    $WshShell = New-Object -ComObject WScript.Shell
+    $shortcut = $WshShell.CreateShortcut($shortcutPath)
+    $shortcut.TargetPath = $target
+    $shortcut.WorkingDirectory = (Split-Path $target -Parent)
+    $shortcut.IconLocation = "$target,0"
+    $shortcut.Save()
+}
+
+function Get-AbsPath {
+    param (
+        $name
+    )
+    
+    $scriptDirectory = $PSScriptRoot
+    $exePath = Join-Path -Path $scriptDirectory -ChildPath $name
+    return $exePath
+}
+
+# Rebind script
+$ahk_script_path = Get-AbsPath -name "desktop_switcher.ahk"
+Start-Process -FilePath $ahk_script_path
+AddToStartup -target $ahk_script_path -name "desktop_switcher"
+
+# GlazeWM
+# C:\Users\samin\scoop\apps\glazewm\current\GlazeWM.exe --config="C:\Users\samin\Documents\Scripts\glazewm-config.yaml"
--- user_config.ahk
+++ user_config.ahk
@@ -165,3 +165,22 @@
 
 ; ^#+Right::MoveCurrentWindowToRightDesktop()
 ; ^#+Left::MoveCurrentWindowToLeftDesktop()
+
+; Move windows between desktop
+; #Left::
+;   WinGetTitle, Title, A
+;   WinSet, ExStyle, ^0x80, %Title%
+;   Send {LWin down}{Ctrl down}{Left}{Ctrl up}{LWin up}
+;   sleep, 50
+;   WinSet, ExStyle, ^0x80, %Title%
+;   WinActivate, %Title%
+; Return
+
+; #Right::
+;   WinGetTitle, Title, A
+;   WinSet, ExStyle, ^0x80, %Title%
+;   Send {LWin down}{Ctrl down}{Right}{Ctrl up}{LWin up}
+;   sleep, 50
+;   WinSet, ExStyle, ^0x80, %Title%
+;   WinActivate, %Title%
+; Return
